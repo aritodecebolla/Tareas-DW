38222a2d77f9b8f49204752efd0372a9e7dc2b59
Arianna Baker Flores <134568130+aritodecebolla@users.noreply.github.com>
2024-06-12 10:56:21 -0600

    Tarea 1 semana 3 Dw

---
+++ src/main/java/com/informate/InformativaApplication.java
@@ -0,0 +1,13 @@
+package com.informate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informate/InformativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
